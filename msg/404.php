bdea81c32bec437aa0d872daa1fc82afa34182fb
fernandorosero <tatofarp@hotmail.com>
2017-11-05 05:48:26 +0100

    lenovo i5

---
+++ msg/404.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Last Monkey</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../ico/FaviconLastMonkey.png">
+        <style>
+            body{text-align: center;}
+                .contenido{width: 100%;height: 100%;color: red;margin-top: 20px;}
+                    .contenido h2{margin-top: 0px;}
+                    .contenido img{width: 120px;height: 130px;}
+        </style>
+    </head>
+    <body>
+        <div class="contenido">
+            <h2>Acceso prohibido</h2>
+            <img src="../img/monkeyoffice.jpg"/>
+            <p>No se encuentra el recurso!!</p>
+        </div>
+    </body>
+</html>
